a80ceae33bb6526a22ac4d8ac4d9e2ad3b76533a
phuthanh99 <huynhnguyenphuthanh@mail.com>
2022-02-24 18:47:32 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+﻿const path = require('path');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// templates engine
+app.engine('hbs', exphbs.engine({
+  extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
